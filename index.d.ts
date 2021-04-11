95b2b5544e0556f91363bdb0146ab571ea306cc7
nghia <nqnghia285@gmail.com>
2021-04-11 12:24:11 +0700

    Setup Package

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+/// <reference path="./index.ts" />
+/// <reference types="socket.io" />
+/// <reference types="socket.io-cookies-parser" />
+
+import { Server } from "http";
+import socketio, { Namespace } from "socket.io";
+
+/**
+ * @method initIO
+ * @param server
+ * @param origin
+ * @returns Server
+ */
+export function initIO(server: Server, origin: string): socketio.Server;
+
+/**
+ * @method createNamespace
+ * @param nsp
+ * @returns Namespace
+ */
+export function createNamespace(nsp: string): Namespace;
---
+++ index.ts
@@ -0,0 +1,37 @@
+import { Server } from "http";
+import socketio, { Namespace } from "socket.io";
+import { parser, cookieParser } from "socket.io-cookies-parser";
+
+const io = new socketio.Server();
+
+/**
+ * @method initIO
+ * @param server
+ * @param origin
+ * @returns Server
+ */
+export function initIO(server: Server, origin: string): socketio.Server {
+    io.attach(server, {
+        cors: {
+            origin: [origin],
+            methods: ["GET", "POST"],
+            allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Origin", "Accept"],
+            credentials: true,
+        },
+    });
+
+    io.use(cookieParser);
+
+    return io;
+}
+
+/**
+ * @method createNamespace
+ * @param nsp
+ * @returns Namespace
+ */
+export function createNamespace(nsp: string): Namespace {
+    return io.of(nsp, parser);
+}
+
+export default io;
